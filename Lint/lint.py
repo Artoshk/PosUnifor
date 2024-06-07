abb7df686ef55ecf820ce06dfb6ea8a7cb06b796
Anderson Karl <42244540+Artoshk@users.noreply.github.com>
2024-06-06 21:05:07 -0300

    Testando hook13

--- Lint/lint.py
+++ Lint/lint.py
@@ -1,12 +1,12 @@
 import time
 
 def main():
-    print("Imagine que ele tá rodando o lint")
+    print("Imagine que ele tá rodando o lint e os testes")
     for i in range(10):
         time.sleep(0.5)
         print('.', end='', flush=True)
     print()
-    print("Lint finalizado com sucesso")
+    print("Lint e testes finalizados com sucesso")
 
 if __name__ == '__main__':
     main()
